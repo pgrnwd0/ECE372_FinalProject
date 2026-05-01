0c689d39c0c8c7d21b3c1be2509e50ff21932e65
Preston <greenwood@arizona.edu>
2026-05-01 12:01:10 -0700

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include "i2c.h"
+
+enum State {OPEN, DEBOUNCE, CLOSED};
+State state;
+
+
+int main(){
+  Serial.begin(115200);
+  initI2C();
+  Write16_to(0x40, 0x00, 0x399F);
+  Write16_to(0x40, 0x05, 0x1061);
+  
+
+  DDRA  |= (1 << PA0);
+  PORTA &= ~(1 << PA0) ; 
+
+  while (1)
+    {
+        int16_t  raw_current = (int16_t)Read_from(0x40, 0x04);
+
+        float   current_mA = raw_current * 0.000977f;         // 0.1mA per bit
+        Serial.print("Current: ");
+        Serial.print(current_mA);
+        Serial.println(" Amps");
+        if (current_mA <= 0.01){
+          PORTA |= (1 << PA0) ;
+          _delay_ms(10); 
+        }
+        else{
+          PORTA &= ~(1 << PA0);
+          _delay_ms(10);
+        }
+
+        _delay_ms(10);
+        
+        
+    }
+
+  return 0;
+}
